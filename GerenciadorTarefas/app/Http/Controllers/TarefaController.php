64d7eb4483b3c5a0879c4e3593ac142cacfda6c7
Aline Avila Brunetti <alinebrunetti88@gmail.com>
2025-08-29 17:18:08 -0300

    Arrumando erros

--- GerenciadorTarefas/app/Http/Controllers/TarefaController.php
+++ GerenciadorTarefas/app/Http/Controllers/TarefaController.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace App\Http\Controllers;
 
 use App\Models\Tarefa;
@@ -7,42 +9,42 @@ class TarefaController extends Controller
 {
     public function index()
     {
-    $tarefas = Tarefa::all();
-    return view('tarefas.index', compact('tarefas'));
+        $tarefas = Tarefa::all();
+        return view('tarefas.index', compact('tarefas'));
     }
 
     public function create()
     {
-    return view('tarefas.create');
+        return view('tarefas.create');
     }
 
     public function store(Request $request)
     {
-    $request->validate(['titulo' => 'required']);
-    Tarefa::create($request->all());
-    return redirect()->route('tarefas.index')->with('success', 'Tarefa criada com sucesso!');
+        $request->validate(['titulo' => 'required']);
+        Tarefa::create($request->all());
+        return redirect()->route('tarefas.index')->with('success', 'Tarefa criada com sucesso!');
     }
 
     public function show(Tarefa $tarefa)
     {
-    return view('tarefas.show', compact('tarefa'));
+        return view('tarefas.show', compact('tarefa'));
     }
 
     public function edit(Tarefa $tarefa)
     {
-    return view('tarefas.edit', compact('tarefa'));
+        return view('tarefas.edit', compact('tarefa'));
     }
 
     public function update(Request $request, Tarefa $tarefa)
     {
-    $request->validate(['titulo' => 'required']);
-    $tarefa->update($request->all());
-    return redirect()->route('tarefas.index')->with('success', 'Tarefa atualizada!');
+        $request->validate(['titulo' => 'required']);
+        $tarefa->update($request->all());
+        return redirect()->route('tarefas.index')->with('success', 'Tarefa atualizada!');
     }
 
     public function destroy(Tarefa $tarefa)
     {
-    $tarefa->delete();
-    return redirect()->route('tarefas.index')->with('success', 'Tarefa excluída!');
+        $tarefa->delete();
+        return redirect()->route('tarefas.index')->with('success', 'Tarefa excluída!');
     }
 }
