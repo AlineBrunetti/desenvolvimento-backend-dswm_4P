8c3b9e075eec30f3aa72c945d3ed21453820821d
Aline Avila Brunetti <alinebrunetti88@gmail.com>
2025-10-20 18:37:17 -0300

    Estendendo a Factory

--- EstendendoAFactory/app/Reports/Factories/ReportFactory.php
+++ EstendendoAFactory/app/Reports/Factories/ReportFactory.php
@@ -5,6 +5,9 @@ namespace App\Reports\Factories;
 use App\Interfaces\ReportInterface;
 use App\Reports\Implementations\CsvReport;
 use App\Reports\Implementations\PdfReport;
+
+use App\Reports\Implementations\JsonReport;
+
 use InvalidArgumentException;
 class ReportFactory
 {
@@ -22,6 +25,10 @@ class ReportFactory
                 return new PdfReport();
             case 'csv':
                 return new CsvReport();
+            
+            case 'json':
+                return new JsonReport();
+
             default:
                 throw new InvalidArgumentException("Tipo de relatório '{$type}' não suportado.");
         }
