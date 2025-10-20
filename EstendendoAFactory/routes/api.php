2deca38405eb5d6dbfd5a67ffe138f3a21d23c59
Aline Avila Brunetti <alinebrunetti88@gmail.com>
2025-10-20 16:00:31 -0300

    arrumando a rota

--- EstendendoAFactory/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// routes/api.php
-use App\Http\Controllers\ReportController;
-// Exemplo de rota: GET /api/reports/pdf ou GET /api/reports/csv
-Route::get('/reports/{type}', [ReportController::class, 'download']);
