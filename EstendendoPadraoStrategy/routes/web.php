c4b04e49722cbbfd0333827d6ad9591d3fc5d69d
Aline Avila Brunetti <alinebrunetti88@gmail.com>
2025-11-03 16:21:19 -0300

    .

--- EstendendoPadraoStrategy/routes/web.php
+++ EstendendoPadraoStrategy/routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\OrderController;
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Rota de exemplo: POST /calculate-discount
-Route::post('/calculate-discount', [OrderController::class, 'calculate']);
