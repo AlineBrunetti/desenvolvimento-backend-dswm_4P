36e31dd58f0d1e440879dcf1c881e72ccda7f3c6
Aline Avila Brunetti <alinebrunetti88@gmail.com>
2025-11-03 15:44:03 -0300

    Upload do exemplo

--- EstendendoPadraoStrategy/routes/web.php
+++ EstendendoPadraoStrategy/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\OrderController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rota de exemplo: POST /api/calculate-discount
+Route::post('/calculate-discount', [OrderController::class, 'calculate']);
